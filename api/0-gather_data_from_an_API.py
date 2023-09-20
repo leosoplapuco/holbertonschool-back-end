7899eac94d549be277e4ad509717f59cf26e2069
Leonardo Soplapuco <leosoplapuco29@gmail.com>
2023-09-20 14:48:16 -0500

    Modified: 0-gather_data_from,_and_API 18

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,35 +1,14 @@
 #!/usr/bin/python3
-""" Getting data from an API """
+"""Getting data from an API"""
 import requests
-from sys import argv
+import sys
 
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
+    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
 
-url = 'https://jsonplaceholder.typicode.com'
-
-response = requests.get(url)
-
-
-def make_request():
-    response_tasks = requests.get(f"{url}/todos?userId={argv[1]}")
-    response_user = requests.get(f"{url}/users/{argv[1]}")
-
-    all_task = response_tasks.json()
-    username = response_user.json().get('name')
-
-    completed_tasks = []
-    for task in all_task:
-        if task.get('completed'):
-            completed_tasks.append(task)
-
+    completed = [t.get("title") for t in todos if t.get("completed") is True]
     print("Employee {} is done with tasks({}/{}):".format(
-        username,
-        len(completed_tasks),
-        len(all_task))
-        )
-
-    for task in completed_tasks:
-        print(f"\t {task.get('title')}")
-
-
-if __name__ == '__main__':
-    make_request()
+        user.get("name"), len(completed), len(todos)))
+    [print("\t {}".format(c)) for c in completed]
