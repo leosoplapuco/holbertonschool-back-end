6ee4f5f57cdb929417b25d2c4b2321d2753050a6
Leonardo Soplapuco <leosoplapuco29@gmail.com>
2023-09-20 12:13:56 -0500

    Add: 1-export_to_CSV

---
+++ api/1-export_to_CSV.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Importing modules"""
+import csv
+import requests
+from sys import argv
+
+if __name__ == '__main__':
+    url_users = requests.get('https://jsonplaceholder.typicode.com/users')
+    url_todos = requests.get('https://jsonplaceholder.typicode.com/todos')
+
+    for i in url_users.json():
+        if i['id'] == int(argv[1]):
+            username = i['username']
+    with open(f"{argv[1]}.csv", 'w') as f:
+        for i in url_todos.json():
+            if i['userId'] == int(argv[1]):
+                completed = i['completed']
+                title = i['title']
+                f.write("\"{}\",\"{}\",\"{}\",\"{}\"\n".
+                        format(argv[1], username, completed, title))
