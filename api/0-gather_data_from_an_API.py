5beb923cd3d59472668197e16c18e3ebe4f0d87c
Leonardo Soplapuco <leosoplapuco29@gmail.com>
2023-09-20 11:31:38 -0500

    Modified: 0-gather_data_from,_and_API 9

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -3,20 +3,18 @@
 import requests
 from sys import argv
 
-
 def todo(userid):
-    name = requests.get(
-        'https://jsonplaceholder.typicode.com/users/{}'.format(
-            userid)).json().get('name')
-    tasks = requests.get(
-        'https://jsonplaceholder.typicode.com/users/{}/todos'.format(
-            userid)).json()
-    tasksDone = ['\t {}\n'.format(dic.get('title')) for dic in tasks
-                 if dic.get('completed')]
-    if name and tasks:
-        print("Employee {} is done with tasks({}/{}):".format
-              (name, len(tasksDone), len(tasks)))
-        print(''.join(tasksDone), end='')
+    user_info = requests.get('https://jsonplaceholder.typicode.com/users/{}'.format(userid)).json()
+    user_name = user_info.get('name')
+    tasks = requests.get('https://jsonplaceholder.typicode.com/users/{}/todos'.format(userid)).json()
+    tasks_done = [dic.get('title') for dic in tasks if dic.get('completed')]
+    
+    if user_name and tasks:
+        num_completed_tasks = len(tasks_done)
+        total_tasks = len(tasks)
+        print("Employee {} is done with tasks ({}/{}):".format(user_name, num_completed_tasks, total_tasks))
+        for task_title in tasks_done:
+            print('\t{}'.format(task_title))
 
 
 if __name__ == "__main__":
