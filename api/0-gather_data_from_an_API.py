4ef1862480673f9c487511a1f9a13ce34694dddd
Leonardo Soplapuco <leosoplapuco29@gmail.com>
2023-09-20 12:02:30 -0500

    Modified: 0-gather_data_from,_and_API 13

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,37 +1,34 @@
 #!/usr/bin/python3
 """ Getting data from an API """
 import requests
-import sys
-
-API_URL = 'https://jsonplaceholder.typicode.com'
+from sys import argv
 
 
-def get_employee_todo_progress(user_id):
-    user_response = requests.get(f"{API_URL}/users/{user_id}")
-    tasks_response = requests.get(f"{API_URL}/todos?userId={user_id}")
+url = 'https://jsonplaceholder.typicode.com'
+response = requests.get(url)
 
-    if user_response.status_code != 200 or tasks_response.status_code != 200:
-        print("Failed to retrieve data from the API.")
-        return
 
-    user_data = user_response.json()
-    tasks_data = tasks_response.json()
+def make_request():
+    response_tasks = requests.get(f"{url}/todos?userId={argv[1]}")
+    response_user = requests.get(f"{url}/users/{argv[1]}")
 
-    username = user_data.get('name')
-    completed_tasks = [task for task in tasks_data if task.get('completed')]
+    all_task = response_tasks.json()
+    username = response_user.json().get('name')
 
-    total_tasks = len(tasks_data)
-    num_completed_tasks = len(completed_tasks)
+    completed_tasks = []
+    for task in all_task:
+        if task.get('completed'):
+            completed_tasks.append(task)
 
-    print(f"Employee {username} is done with tasks ({num_completed_tasks}/{total_tasks}):")
+    print("Employee {} is done with tasks({}/{}):".format(
+        username,
+        len(completed_tasks),
+        len(all_task))
+        )
 
     for task in completed_tasks:
-        print(f"\t{task['title']}")
+        print(f"\t {task.get('title')}")
 
 
 if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        print("Usage: python script.py <user_id>")
-    else:
-        user_id = int(sys.argv[1])
-        get_employee_todo_progress(user_id)
+    make_request()
