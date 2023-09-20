6f0f55bfad2adbe13fff971ca560801234aea272
Leonardo Soplapuco <leosoplapuco29@gmail.com>
2023-09-20 11:41:59 -0500

    Modified: 0-gather_data_from,_and_API 10

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,20 +1,16 @@
 #!/usr/bin/python3
-""" Getting data from an API """
+"""gets api"""
 import requests
 from sys import argv
 
+
 def todo(userid):
-    user_info = requests.get('https://jsonplaceholder.typicode.com/users/{}'.format(userid)).json()
-    user_name = user_info.get('name')
+    name = requests.get('https://jsonplaceholder.typicode.com/users/{}'.format(userid)).json().get('name')
     tasks = requests.get('https://jsonplaceholder.typicode.com/users/{}/todos'.format(userid)).json()
-    tasks_done = [dic.get('title') for dic in tasks if dic.get('completed')]
-    
-    if user_name and tasks:
-        num_completed_tasks = len(tasks_done)
-        total_tasks = len(tasks)
-        print("Employee {} is done with tasks ({}/{}):".format(user_name, num_completed_tasks, total_tasks))
-        for task_title in tasks_done:
-            print('\t{}'.format(task_title))
+    tasksDone = ['\t {}\n'.format(dic.get('title')) for dic in tasks if dic.get('completed')]
+    if name and tasks:
+        print("Employee {} is done with tasks({}/{}):".format(name, len(tasksDone), len(tasks)))
+        print(''.join(tasksDone), end='')
 
 
 if __name__ == "__main__":
