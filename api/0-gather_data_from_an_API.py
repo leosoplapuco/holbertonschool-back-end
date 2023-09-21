d4941a269a252c0544f3d0886676c433b3727c45
Leonardo Soplapuco <leosoplapuco29@gmail.com>
2023-09-21 10:14:56 -0500

    Modified: 0-gather_data_from,_and_API 23

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -4,15 +4,33 @@ import requests
 import sys
 
 
-try:
-    user = requests.get("https://jsonplaceholder.typicode.com/users/{}".format(sys.argv[1])).json()
-    todos = requests.get("https://jsonplaceholder.typicode.com/todos", params={"userId": sys.argv[1]}).json()
-except Exception as e:
-    print(e)
-    sys.exit(1)
+def get_employee_todo_list_progress(employee_name):
+    response = requests.get("https://jsonplaceholder.typicode.com/users/{}".format(employee_name))
 
-completed = [t.get("title") for t in todos if t.get("completed") is True]
+    if response.status_code != 200:
+        raise Exception("Failed to get employee TODO list progress: {}".format(response.status_code))
 
-print("Employee {} is done with tasks({}/{}):".format(user.get("name"), len(completed), len(todos)))
-for c in completed:
-    print("\t {}".format(c))
+    user = response.json()
+
+    todos = requests.get("https://jsonplaceholder.typicode.com/todos", params={"userId": user.get("id")}).json()
+    completed = [t.get("title") for t in todos if t.get("completed") is True]
+
+    return {
+        "employee_name": user.get("name"),
+        "number_of_done_tasks": len(completed),
+        "total_number_of_tasks": len(todos),
+    }
+
+
+if __name__ == "__main__":
+    employee_name = input("Enter the employee name: ")
+
+    employee_todo_list_progress = get_employee_todo_list_progress(employee_name)
+
+    print("Employee {} is done with tasks({}/{}):".format(
+        employee_todo_list_progress["employee_name"],
+        employee_todo_list_progress["number_of_done_tasks"],
+        employee_todo_list_progress["total_number_of_tasks"],
+    ))
+    for c in employee_todo_list_progress["completed_tasks"]:
+        print("\t {}".format(c))
