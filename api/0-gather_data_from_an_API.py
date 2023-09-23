7a48cdc37434933071c02e68b2e8d14afb5d72e0
Leonardo Soplapuco <leosoplapuco29@gmail.com>
2023-09-22 22:45:50 -0500

    Modified: 0-gather_data_from,_and_API 31

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -4,12 +4,46 @@ import requests
 import sys
 
 
-if __name__ == "__main__":
-    url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
-    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
+API = 'https://jsonplaceholder.typicode.com'
 
-    completed = [t.get("title") for t in todos if t.get("completed") is True]
-    print("Employee {} is done with tasks({}/{}):".format(
-        user.get("name"), len(completed), len(todos)))
-    [print("\t {}".format(c)) for c in completed]
+
+endpoints = {
+        "users": f"{API}/users",
+        "todos": f"{API}/todos"
+        }
+
+
+def get_todo_list_progress(employee_id: int) -> str:
+    get_employee_endpoint = f"{endpoints['users']}/{employee_id}"
+    employee_response = requests.get(get_employee_endpoint)
+    employee = employee_response.json()
+
+    get_employee_todos_endpoint = f"{endpoints['todos']}?userId={employee_id}"
+    todos_response = requests.get(get_employee_todos_endpoint)
+    todos = todos_response.json()
+
+    to_len = len(todos)
+    
+    completed_todos = []
+
+    for todo in todos:
+        if todo["completed"] is True:
+            completed_todos.append(todo)
+    com_to_qu = len(completed_todos)
+
+    text = "is done with tasks"
+    first_line = f"Employee {employee['name']} {text}({com_to_qu}/{to_len}):\n"
+
+    next_line = ''
+    for completed_todo in completed_todos:
+        next_line = next_line + "\t " + completed_todo["title"] + "\n"
+
+    return first_line + next_line
+
+
+if __name__ == '__main__':
+    employee_id = sys.argv[1]
+
+    output = get_todo_list_progress(employee_id)
+
+    print(output, end="")
