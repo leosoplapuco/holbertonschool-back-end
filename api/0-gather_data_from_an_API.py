7be2de2b21ce5c2da21d42eeece928ab158c5068
Leonardo Soplapuco <leosoplapuco29@gmail.com>
2024-01-19 11:08:23 -0500

    Update: Api directory

---
+++ api/0-gather_data_from_an_API.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+import requests
+import sys
+
+
+if __name__ == '__main__':
+
+    id = sys.argv[1]
+    task_title = []
+    complete = 0
+    total = 0
+    url_user = "https://jsonplaceholder.typicode.com/users/" + id
+    result = requests.get(url_user).json()
+    name = result.get('name')
+    todos = "https://jsonplaceholder.typicode.com/todos/"
+    res_task = requests.get(todos).json()
+    for i in res_task:
+        if i.get('userId') == int(id):
+            if i.get('completed') is True:
+                task_title.append(i['title'])
+                complete += 1
+            total += 1
+    print("Employee {} is done with tasks({}/{}):"
+          .format(name, complete, total))
+    for x in task_title:
+        print("\t {}".format(x))
---
+++ api/1-export_to_CSV.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import csv
+import requests
+import sys
+
+
+if __name__ == '__main__':
+    id = sys.argv[1]
+    url_user = "https://jsonplaceholder.typicode.com/users/" + id
+    res = requests.get(url_user).json()
+    u = res.get("username")
+    req = requests.get('https://jsonplaceholder.typicode.com/users/' + (id) + '/todos')
+    with open("{}.csv".format(id), "w") as f:
+        writer = csv.writer(f, quoting=csv.QUOTE_ALL)
+        for t in req.json():
+            writer.writerow([id, u, t.get("completed"), t.get("title")])
