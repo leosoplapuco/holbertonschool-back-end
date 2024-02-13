e5dad03935970695e240cd96e20d8074ffa20e81
leosoplapuco <leosoplapuco29@gmail.com>
2024-02-13 16:30:52 -0500

    Mod: 0-gather_data_from_an_API - Task 00 4

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -12,7 +12,8 @@ if __name__ == '__main__':
     url_user = "https://jsonplaceholder.typicode.com/users/" + id
     res = requests.get(url_user).json()
     u = res.get("username")
-    req = requests.get('https://jsonplaceholder.typicode.com/users/' + (id) + '/todos')
+    req = requests.get('https://jsonplaceholder.typicode.com/users/' + 
+                       (id) + '/todos')
     with open("{}.csv".format(id), "w") as f:
         writer = csv.writer(f, quoting=csv.QUOTE_ALL)
         for t in req.json():
